9dd6aad9a6495353ea4c8d4ddd07dbfd0bea703d
Atakan Celebisoy <AtakanCelebisoy@atakan-macbook-air.local>
2016-08-13 11:57:27 +0300

    Initial Commit

---
+++ destroyTheWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  destroyTheWorld
+//
+//  Created by Atakan Celebisoy on 13.08.2016.
+//  Copyright © 2016 Atakan Celebisoy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
